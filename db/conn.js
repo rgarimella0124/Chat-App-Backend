69929bf189d40903dff5b90ba2e97c8042ae6067
Ram Simran <ram@payzello.in>
2020-01-10 13:44:46 +0530

    Working

--- db/conn.js
+++ db/conn.js
@@ -1,29 +1,26 @@
 const Sequelize = require("sequelize");
 // const dotenv = require('dotenv');
-var conn = new Sequelize(
-  "postgres://postgresRam:helloworld12@seconddb.cykw6tqn03sy.ap-south-1.rds.amazonaws.com:5432",
-  {
-    retry: {
-      match: [
-        /ETIMEDOUT/,
-        /EHOSTUNREACH/,
-        /ECONNRESET/,
-        /ECONNREFUSED/,
-        /ETIMEDOUT/,
-        /ESOCKETTIMEDOUT/,
-        /EHOSTUNREACH/,
-        /EPIPE/,
-        /EAI_AGAIN/,
-        /SequelizeConnectionError/,
-        /SequelizeConnectionRefusedError/,
-        /SequelizeHostNotFoundError/,
-        /SequelizeHostNotReachableError/,
-        /SequelizeInvalidConnectionError/,
-        /SequelizeConnectionTimedOutError/
-      ],
-      max: 5
-    }
+var conn = new Sequelize("Your Connection URL", {
+  retry: {
+    match: [
+      /ETIMEDOUT/,
+      /EHOSTUNREACH/,
+      /ECONNRESET/,
+      /ECONNREFUSED/,
+      /ETIMEDOUT/,
+      /ESOCKETTIMEDOUT/,
+      /EHOSTUNREACH/,
+      /EPIPE/,
+      /EAI_AGAIN/,
+      /SequelizeConnectionError/,
+      /SequelizeConnectionRefusedError/,
+      /SequelizeHostNotFoundError/,
+      /SequelizeHostNotReachableError/,
+      /SequelizeInvalidConnectionError/,
+      /SequelizeConnectionTimedOutError/
+    ],
+    max: 5
   }
-);
+});
 
 module.exports = conn;
