ec04d7776e36be38be16051140aa10e0307ad8fa
Ram Simran <ram@payzello.in>
2020-01-10 13:27:34 +0530

    README Added and also nodemon

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,5 @@
 const Sequelize = require("sequelize");
 // const dotenv = require('dotenv');
-// dotenv.config();
 var conn = new Sequelize(
   "postgres://postgresRam:helloworld12@seconddb.cykw6tqn03sy.ap-south-1.rds.amazonaws.com:5432",
   {
